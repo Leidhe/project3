61593f8b84ff88a46c6851511f7a8a1a9993ede9
Leidhe <moroma.monica@gmail.com>
2020-06-06 14:51:29 +0200

    Templates modified and fixed

--- orders/urls.py
+++ orders/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("about", views.about, name="about"),
+    path("menu", views.menu, name="menu"),
+    path("contact", views.contact, name="contact"),
+    path("cart", views.cart, name="cart"),
+
 ]
--- orders/views.py
+++ orders/views.py
@@ -3,4 +3,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Project 3: TODO")
+    return render(request, "orders/index.html")
+
+def about(request):
+    return render(request, "orders/about.html")
+
+def menu(request):
+    return render(request, "orders/menu.html")
+
+def contact(request):
+    return render(request, "orders/contact.html")
+
+def cart(request):
+    return render(request, "orders/cart.html")
